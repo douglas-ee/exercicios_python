3e803d48cd2b7a6373344a55294d77c69182d88b
Douglas Gomes <douglas.gomes@ee.ufcg.edu.br>
2020-06-18 09:51:31 -0300

    ~...

---
+++ introducao_python/2_input.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# AQUI VAMOS ENTENDER O CONCEITO DE INPUT E COMO USA-LO
+
+Nome = input("Digite o seu nome: ")
+Idade = input("Digite sua idade: ")
+Sexo = input("Digite o seu Sexo: ")
+
+print("Nome é: " + Nome)
+print("Idade é: " + Idade)
+print("Sexo é: " + Sexo)
---
+++ introducao_python/3_variaveis.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# AQUI IREMOS ENTENDER UM POUCO MELHOR O CONCETO DE VARIAVEIS EM PYTHON
+
+texto_1 = "Olá Mundo"
+texto_2 = 'Olá Brasil'
+texto_3 = 'Operações matematicas a seguir'
+
+Soma = 2 + 3
+Subtracao = 2 - 3
+Multiplicacao = 2 * 3
+Divisao = 2 / 3
+Potencia = 2 ** 3
+Resto = 2 % 3
+
+print(texto_1)
+print(texto_2)
+print(texto_3)
+print(Soma)
+print(Subtracao)
+print(Multiplicacao)
+print(Divisao)
+print(Potencia)
+print(Resto)
+
+print(type(texto_2))
