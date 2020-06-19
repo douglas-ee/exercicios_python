244b71949139d87da956049ddf1b0bef9a848077
Douglas Gomes <douglas.gomes@ee.ufcg.edu.br>
2020-06-19 09:52:39 -0300

    ~...

---
+++ introducao_python/4_operacoes.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# AQUI IREMOS REALIZAR OPERACOES MATEMATICAS DE DIVERSAS FORMAS
+
+print('A Somas é: ', 2 + 3)
+print('A Subtração é: ', 2 - 3)
+print('A Multiplicação é: ', 2 * 3)
+print('A Divisão é: ', 2 / 3)
+print('A Potência é: ', 2 ** 3)
+print('O Resto da Divisão é: ', 2 % 3)
---
+++ introducao_python/5_operadores.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# AQUI VAMOS ENTENDER MAIS SOBRE OPERADORES EM PYTHON
+
+A = 1
+B = 2
+C = 1
+
+print(A == B)
+print(A == C)
+print(A == B and A == C)
+print(A == B or A == C)
+print(A >= B and A <= C)
+print(B >= A and B >= C)
+print(not not not A == C)
