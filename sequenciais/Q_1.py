efe43526f28508477971d8d66010afd43e387f72
douglas-ee <douglas.gomes@ee.ufcg.edu.br>
2020-06-22 09:38:59 -0300

    ~...

---
+++ sequenciais/Q_1.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+# Faça um Programa que mostre a mensagem "Alo mundo" na tela
+
+print('Alo Mundo')
---
+++ sequenciais/Q_3.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# Faça um Programa que peça dois números e imprima a soma
+
+Numero_1 = int(input("Digite o Primeiro Numero: "))
+Numero_2 = int(input("Digite o Segundo Numero: "))
+
+Soma = Numero_1 + Numero_2
+
+print("A soma dos numeros {} e {} é: {}".format(Numero_1, Numero_2, Soma))
