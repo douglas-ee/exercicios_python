3d748b6e55d35403cf14447138fb57bf7c6332fe
douglas-ee <douglas.gomes@ee.ufcg.edu.br>
2020-06-23 13:18:26 -0300

    ~...

---
+++ repeticao/Q_5.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Altere o programa anterior permitindo ao usuário informar as populações e as
+# taxas de crescimento iniciais. Valide a entrada e permita repetir a operacao.
+
+Ano = 0
+
+P_A = float(input("População da cidade A: "))
+Taxa_A = float(input("Taxa de crescimento da população da cidade A: "))
+
+P_B = float(input("População da cidade B: "))
+Taxa_B = float(input("Taxa de crescimento da população da cidade B: "))
+
+while P_A < P_B:
+    P_A += ((P_A*Taxa_A)/100)
+    P_B += ((P_B*Taxa_B)/100)
+    Ano = Ano+1
+
+print("levará", Ano, "Anos para população da cidade A ser maior que a cidade B")
+print("P_B: ", P_B, "habitantes")
+print("P_A: ", P_A, "habitantes")
---
+++ repeticao/Q_7.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Faca um programa que leia 5 números e informe o maior numero.
+
+N_1 = int(input("Digite o 1º numero: "))
+N_2 = int(input("Digite o 2º numero: "))
+N_3 = int(input("Digite o 3º numero: "))
+N_4 = int(input("Digite o 4º numero: "))
+N_5 = int(input("Digite o 5º numero: "))
+
+if (N_1 > N_2 and N_1 > N_3 and N_1 > N_4 and N_1 > N_5):
+    print("O maior numero é o 1º: ", N_1)
+
+elif (N_2 > N_1 and N_2 > N_3 and N_2 > N_4 and N_2 > N_5):
+    print("O maior numero é o 2º: ", N_2)
+
+elif (N_3 > N_2 and N_3 > N_1 and N_3 > N_4 and N_3 > N_5):
+    print("O maior numero é o 3º: ", N_3)
+
+elif (N_4 > N_2 and N_4 > N_3 and N_4 > N_1 and N_4 > N_5):
+    print("O maior numero é o 4º: ", N_4)
+
+else:
+    print("O maior numero é o 5º: ", N_5)
